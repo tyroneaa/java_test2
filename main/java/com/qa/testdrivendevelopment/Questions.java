effae3b143052d0e433d1cd7e07b8381a75f6d60
Tyrone <tyrone.appiah@academytrainee.com>
2020-04-17 14:20:49 +0100

    java test 2

---
+++ main/java/com/qa/testdrivendevelopment/Questions.java
@@ -0,0 +1,223 @@
+package com.qa.testdrivendevelopment;
+
+public class Questions {
+	/**
+	 * EXAMPLE: THIS ONE HAS BEEN DONE FOR YOU <br>
+	 * 
+	 * Given a name, return "Hi" plus the name <br>
+	 * <br>
+	 * For Example:<br>
+	 * greetingExample("John") → "Hi John" <br>
+	 * greetingExample("Matt") → "Hi Matt" <br>
+	 * greetingExample("Angelica") → "Hi Angelica"
+	 * 
+	 */
+	public String greetingExample(String name) {
+		return "Hi " + name;
+	}
+
+
+	/**
+	 * Given a string and a char, returns the position in the String where the char first appears.
+	 * Ensure the positions are numbered correctly, please refer to the examples for guidance. <br>
+	 * Do not ignore case <br>
+	 * Ignore whitespace <br>
+	 * If the char does not occur, return the number -1. <br>
+	 * <br>
+	 * For Example: <br>
+	 * returnPosition("This is a Sentence",'s') → 4 <br>
+	 * returnPosition("This is a Sentence",'S') → 8 <br>
+	 * returnPosition("Fridge for sale",'z') → -1
+	 */
+	public int returnPosition(String input, char letter) {
+		return -1;
+	}
+
+	/**
+	 * Given two Strings of equal length, 'merge' them into one String.
+	 * Do this by 'zipping' the Strings together. <br>
+	 * Start with the first char of the first String. <br>
+	 * Then add the first char from the second String. <br>
+	 * Then add the second char from the first String. <br>
+	 * And so on. <br>
+	 * Maintain case. <br>
+	 * You will not encounter whitespace. <br>
+	 * <br>
+	 * For Example: <br>
+	 * zipped("String","Fridge") → "SFtrriidngge" <br>
+	 * zipped("Dog","Cat") → "DCoagt"<br>
+	 * zipped("True","Tree") → "TTrrueee" <br>
+	 * zipped("return","letter") → "rleettutrenr" <br>
+	 */
+	public String zipped(String input1, String input2) {
+		StringBuilder sb = new StringBuilder();
+		//String output = "";
+		for(int i = 0; i < input1.length(); i++) {
+			for(int j = 0; j < input2.length(); j++) {
+				sb.append(input1.charAt(i));
+				sb.append(input2.charAt(j));
+			}
+			
+		}
+		return sb.toString();
+	}
+
+	
+	/**
+	 * Given an Array of Strings, remove any duplicate Strings that occur, then return the Array. <br>
+	 * Do not ignore case. <br>
+	 * <br>
+	 * For Example: <br>
+	 * removeDuplicates({"Dog"}) → {"Dog"} <br>
+	 * removeDuplicates({"Dog","Cat"}) → {"Dog","Cat"} <br>
+	 * removeDuplicates({"Dog","Dog","Cat"}) → {"Dog","Cat"} <br>
+	 * removeDuplicates({"Dog","DoG","Cat"}) → {"Dog","DoG","Cat"} 
+	 */
+	public String[] removeDuplicates(String[] stringArray) {
+		//String newArr[];
+		//newArr[0] = stringArray[0];
+		
+		for(int i = 0; i < stringArray.length; i++) {
+			
+		}
+		return null;
+	}
+
+	/**
+	 * Given a large string that represents a csv (comma separated values), the structure of each record will be as follows:
+	 * owner,nameOfFile,encrypted?,fileSize <br>
+	 * <br>
+	 * Example:
+	 * "Bert,private.key,True,1447\nBert,public.key,False,1318\nJeff,private.key,False,1445" <br>
+	 * <br>
+	 * Where a comma separates out the fields, and the \n represents a new line. <br>
+	 * For each record, if it is not encrypted, return the names of the owners of the files in a String Array. <br>
+	 * Do not include duplicate names. <br>
+	 * <br>
+	 * For Example: <br>
+	 * csvScan("Jeff,private.key,False,1445") → {"Jeff"} <br>
+	 * csvScan("Bert,private.key,True,1447\nBert,public.key,True,1318\nJeff,private.key,False,1445") → {"Jeff"} <br>
+	 * csvScan("Bert,private.key,False,1447\nBert,public.key,False,1318\nJeff,private.key,False,1445") → {"Bert","Jeff"} <br>
+	 * csvScan("Bert,private.key,True,1447\nBert,public.key,False,1318\nJeff,private.key,False,1445") → {"Bert","Jeff"}
+	 */
+	public String[] csvScan(String csvInput) {
+		return null;
+	}
+
+	
+	/**
+	 * Write a function to randomly generate a list with 5 even numbers between 100 and 200 inclusive. <br>
+	 *  <br>
+	 * For Example:     <br>
+	 * listGen() → {100,102,122,198,200}  <br>
+	 * listGen() → {108,104,106,188,200}  <br>
+	 * listGen() → {154,102,132,178,164}
+	 */
+	public int[] listGen() {
+		return new int[] {0,0,0,0,0};
+		
+	}
+
+
+	/**
+	 * A prime number is one which is only divisible by one and itself.  <br>
+	 * Write a function which returns the boolean True if a number is prime, and the boolean False if not. <br>
+	 *  <br>
+	 * For Example: <br>
+	 * isPrime(3) → True <br>
+	 * isPrime(8) → False
+	 */
+	public boolean isPrime(int num) {
+		boolean prime = false;
+		for (int i = 2; i < num/2; i++) {
+			if (num % i != 0) {
+				prime =  false;
+			}else {
+				prime =  true;
+			}
+		}
+		return prime;
+	}
+		
+
+	/**
+	 * Wrtie a function which returns the number of vowels in a given string.  <br>
+	 * You should ignore case. <br>
+	 *  <br>
+	 * For Example: <br>
+	 * getVowel("Hello") → 2  <br>
+	 * getVowel("hEelLoooO") → 6
+	 */
+	public int getVowel(String input) {
+		int count = 0;
+		//input.toLowerCase();
+		for (int i =0; i < input.length(); i++) {
+			char letter = input.charAt(i);
+			if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	/**
+	 * Given two string inputs, if one can be made from the other return the boolean True, if not return the boolean False.  <br>
+	 *  <br>
+	 * For Example:  <br>
+	 * wordFinder("dog", "god") → True  <br>
+	 * wordFinder("tiredest", "tree") → True  <br>
+	 * wordFinder("dog", "cat") → False  <br>
+	 * wordFinder("tripping", "gin") → True
+	 */
+	public boolean wordFinder(String initialWord, String madeString) {
+		
+		return false;
+	}
+	
+
+	/**
+	 * There is a well known mnemonic which goes "I before E, except after C", which is used to determine which order "ei" or "ie" should be in a word.  <br>
+	 *  <br>
+	 * Write a function which returns the boolean True if a string follows the mnemonic, and returns the boolean False if not.  <br>
+	 *  <br>
+	 * For Example:  <br>
+	 * iBeforeE("ceiling") → True  <br>
+	 * iBeforeE("believe") → True  <br>
+	 * iBeforeE("glacier") → False  <br>
+	 * iBeforeE("height") → False
+	 */
+	public boolean iBeforeE(String input) {
+		if (input.contains("c")) {
+			int index = input.indexOf("c");
+			if (input.charAt(index + 1) == 'i') {
+				return false;
+			}
+				else {
+					return true;
+				}
+	
+		}
+		return false;
+	}
+
+	/**
+	 * 	A factorial is the product of itself and all previous numbers  <br>
+	 * eg 3 factorial is 1 x 2 x 3 = 6  <br>
+	 *  <br>
+	 * Write a function which can compute the factorial of a given number between 1 and 10 inclusive.   <br>
+	 *   <br>
+	 * For Example:  <br>
+	 * factorial(1) → 1  <br>
+	 * factorial(4) → 24  <br>
+	 * factorial(8) → 40320
+	 */
+	public int factorial(int number) {
+		int fact = 1;
+		for(int i = 1; i <= number; i++) {
+			fact = fact * i;
+			
+		}
+		return fact;
+	}
+
+}
